4db723a59a7c9e93571fa968bad3693cfbc41438
Praneeth Sarode <praneethsarode@gmail.com>
2024-11-11 02:13:13 +0530

    remove deprecated functions and fix base64 decoding
    Signed-off-by: Praneeth Sarode <praneethsarode@gmail.com>

--- src/lib/environment.rs
+++ src/lib/environment.rs
@@ -5,13 +5,13 @@ use serde_derive::Deserialize;
 use crate::errors::*;
 
 #[derive(Deserialize, Clone)]
-pub struct TempEnvirontment {
+pub struct TempEnvironment {
     pub ssh_host_username: String,
     pub ssh_key: String,
 }
 
-pub fn read_temp_env(path: &str) -> Result<TempEnvirontment> {
+pub fn read_temp_env(path: &str) -> Result<TempEnvironment> {
     let toml_str = fs::read_to_string(path)?;
-    let env: TempEnvirontment = toml::from_str(&toml_str)?;
+    let env: TempEnvironment = toml::from_str(&toml_str)?;
     Ok(env)
 }
