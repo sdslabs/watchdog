81decb180969a984547059dcb74611b2669f8eba
ayushka11 <gargayushka@gmail.com>
2024-12-21 14:22:20 +0530

    change ssh_env
    Signed-off-by: ayushka11 <gargayushka@gmail.com>

--- src/ssh.rs
+++ src/ssh.rs
@@ -19,12 +19,19 @@ pub fn handle_ssh() -> Result<()> {
         let config = read_config()?;
         init(&config)?;
 
-        let env = read_temp_env("/opt/watchdog/ssh_env")?;
+        let machine_username = env::var("PAM_USER")
+            .chain_err(|| "PAM_USER not set. This should be the username of the ssh user")?;
+        println!("Machine username: {}", machine_username); //I hope this is the username of the machine
+
+        // let env = read_temp_env("/opt/watchdog/ssh_env")?;
+        // let name = get_name(&config, &env.ssh_key)?;
+        let ssh_env_file = format!("/opt/watchdog/{}_ssh_env", machine_username); //machine_ssh_env
+        let env = read_temp_env(&ssh_env_file)?;
         let name = get_name(&config, &env.ssh_key)?;
 
         match fork() {
             Ok(ForkResult::Parent { .. }) => {
-                clear_file("/opt/watchdog/ssh_env")?;
+                clear_file(&ssh_env_file)?;
             }
             Ok(ForkResult::Child) => {
                 notifier::post_ssh_summary(&config, true, name, env.ssh_host_username)?;
