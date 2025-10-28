8a8fd0a1a0f3c225de1b26f89834b67c3dd9f29a
Satindra Ramalingam <satindra.r@gmail.com>
2025-10-28 19:33:17 +0530

    add local cache while updating and authentication
    add caching check during auth process and use watchdog utils' caching
    logic during update
    
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>
    
    cache path in config variables
    
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>
    
    stable for testing (providing cache_path to watchdog utils)
    
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>
    
    to test on centos
    
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>
    
    undo testing changes
    
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>
    
    move check_local_cache from auth.rs to lib/utils.rs
    
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>
    
    config.rs changes for PathBuf
    
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>
    
    directly sending keyhouse struct to watchdog-utils in handle_update
    
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>
    
    remove warn for error logs
    
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>

--- src/auth.rs
+++ src/auth.rs
@@ -1,12 +1,12 @@
-use lib::logger::LogTarget;
-use log::{error, info};
-use nix::unistd::{fork, ForkResult};
-
 use lib::config::read_config;
-use lib::errors::*;
+use lib::errors::{Error, *};
 use lib::init::init;
 use lib::keyhouse::{get_name, validate_user};
+use lib::logger::LogTarget;
 use lib::notifier;
+use lib::utils::check_local_cache;
+use log::info;
+use nix::unistd::{fork, ForkResult};
 
 #[cfg(feature = "auto-update")]
 use crate::update::handle_update;
@@ -35,24 +35,30 @@ pub fn handle_auth(ssh_host_username: &str, ssh_key: &str) -> Result<()> {
             println!("{ssh_key}");
             Ok(())
         }
-
-        Ok(false) => {
-            info!(target: LogTarget::AUTH.as_str(), "User not validated");
-            let name = get_name(&config, ssh_key)?;
-            info!(target: LogTarget::AUTH.as_str(), "Logging failed");
-            match fork() {
-                Ok(ForkResult::Parent { .. }) => {}
-                Ok(ForkResult::Child) => {
-                    notifier::post_ssh_summary(&config, false, &name, ssh_host_username)?;
-                    std::process::exit(0);
+        Ok(false) | Err(_) => {
+            // Try local cache first on validation failure or mismatch
+            if check_local_cache(&config, ssh_host_username, ssh_key) {
+                info!(target: LogTarget::AUTH.as_str(), "User validated from local cache");
+                println!("{ssh_key}");
+                Ok(())
+            } else {
+                info!(target: LogTarget::AUTH.as_str(), "User not validated");
+                let name = get_name(&config, ssh_key)?;
+                match fork() {
+                    Ok(ForkResult::Parent { .. }) => {}
+                    Ok(ForkResult::Child) => {
+                        notifier::post_ssh_summary(
+                            &config,
+                            false,
+                            &name,
+                            ssh_host_username,
+                        )?;
+                        std::process::exit(0);
+                    }
+                    Err(_) => println!("Fork failed"),
                 }
-                Err(_) => println!("Fork failed"),
+                Err(Error::from("User validation failed and not found in cache"))
             }
-            Ok(())
-        }
-        Err(e) => {
-            error!(target: LogTarget::AUTH.as_str(), "Error while validating user from keyhouse");
-            Err(e).chain_err(|| "Error while validating user from keyhouse")
         }
     }
 }
--- src/update.rs
+++ src/update.rs
@@ -2,18 +2,21 @@ use lib::config::read_config;
 use lib::errors::*;
 use lib::init::init;
 use lib::logger::LogTarget;
+use watchdog_utils_II::config::{Config as UtilsConfig, KeyhouseConf as UtilsKeyhouseConf};
 use watchdog_utils_II::services::github_service;
 
 pub fn handle_update() -> Result<()> {
     let config = read_config()?;
     init(&config)?;
-    let keyhouse_config = config.keyhouse;
-    let hostname = config.hostname;
+    let utils_keyhouse = UtilsKeyhouseConf {
+        base_url: config.keyhouse.base_url.clone(),
+        token: config.keyhouse.token.clone(),
+    };
+    let utils_config = UtilsConfig::new(config.hostname.clone(), utils_keyhouse, config.cache_path);
     let rt = tokio::runtime::Runtime::new()?;
     if let Err(e) = rt.block_on(github_service::process_update_request(
-        keyhouse_config,
+        utils_config,
         LogTarget::UPDATE.as_str(),
-        hostname,
     )) {
         return Err(e.to_string().into());
     }
